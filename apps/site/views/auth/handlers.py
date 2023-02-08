a4c968d482989a862d187d5e6501656466d520ac
cyrox007 <cyrox007@gmail.com>
2023-02-08 12:39:08 +0300

    fixed bugs

--- apps/site/views/auth/handlers.py
+++ apps/site/views/auth/handlers.py
@@ -1,21 +1,23 @@
 from fastapi import Request
 
-from apps.site.components.users.decorators import get_session
+""" from apps.site.components.users.decorators import get_session """
+from database import Database
 from apps.site.components.users.model import User
 
 
-@get_session
-async def authorization(request: Request, db_session = None):
+async def authorization(request: Request = None):
+    db_session = Database.connect_database()
     data = await request.json()
-    return { "data": data }
-    """ user_data = User.get_user(db_session, data["username"]) """
-    """ if user_data.username is not None and data["password"] == user_data.password:
+    """ return { "data": data } """
+    user_data = User.get_user(db_session, data["username"])
+    if user_data is not None and data["password"] == user_data.password:
         return { "status": "ok" }
     else:
-        return { "status": "bed" } """
+        return { "status": "bed" }
 
-@get_session
-async def registration(request: Request, db_session = None):
+
+async def registration(request: Request):
+    db_session = Database.connect_database()
     data = await request.json()
     if User.insert_new_user(db_session, data) is not None:
         return { "status": "ok" }
