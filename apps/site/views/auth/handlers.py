f197062af59ae4177983da6f2309d4e69525a92e
cyrox007 <cyrox007@gmail.com>
2023-02-09 16:57:15 +0300

    database conn

--- apps/site/views/auth/handlers.py
+++ apps/site/views/auth/handlers.py
@@ -6,14 +6,13 @@ from apps.site.components.users.model import User
 
 
 async def authorization(request: Request = None):
-    """ db_session = Database.connect_database() """
+    db_session = Database.connect_database()
     data = await request.json()
-    return { "data": data }
-    """ user_data = User.get_user(db_session, data["username"])
+    user_data = User.get_user(db_session, data["username"])
     if user_data is not None and data["password"] == user_data.password:
         return { "status": "ok" }
     else:
-        return { "status": "bed" } """
+        return { "status": "bed" }
 
 
 async def registration(request: Request):
