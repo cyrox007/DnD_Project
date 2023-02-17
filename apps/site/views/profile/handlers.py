8d1a9ef32dc3900a245bc65fa6f94e184a41c903
cyrox007 <cyrox007@gmail.com>
2023-02-17 15:32:17 +0300

    add profile

---
+++ apps/site/views/profile/handlers.py
@@ -0,0 +1,13 @@
+from fastapi import Request
+
+from database import Database
+from apps.site.components.users.model import User
+
+async def get_profile(request: Request = None):
+    response = request.json()
+    db_session = Database.connect_database()
+    user_data = User.get_user(db_session, response["username"])
+    return { 
+        "status": "ok",
+        "user": user_data
+        }
---
+++ apps/site/views/profile/router.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, APIRouter, status
+from apps.site.views.profile import handlers
+
+
+def install(app: FastAPI):
+    router = APIRouter(tags=["server.profile"], prefix="/profile")
+    router.add_api_route(
+        "/",
+        methods=["POST"],
+        status_code=status.HTTP_200_OK,
+        endpoint=handlers.get_profile
+    )
