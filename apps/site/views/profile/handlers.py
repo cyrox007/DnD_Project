51f9e862f0c0bd4b7a34fa02f24c6ee26d607bda
cyrox007 <cyrox007@gmail.com>
2023-02-17 15:46:17 +0300

    fixed view

--- apps/site/views/profile/handlers.py
+++ apps/site/views/profile/handlers.py
@@ -4,8 +4,7 @@ from database import Database
 from apps.site.components.users.model import User
 
 async def get_profile(request: Request = None):
-    response = request.json()
-    print(response)
+    response = await request.json()
     db_session = Database.connect_database()
     user_data = User.get_user(db_session, response["username"])
     return { 
