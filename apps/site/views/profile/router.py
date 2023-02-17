2103b3d74e98f85a1658ac7d4010e3976c171984
cyrox007 <cyrox007@gmail.com>
2023-02-17 15:37:57 +0300

    fix

--- apps/site/views/profile/router.py
+++ apps/site/views/profile/router.py
@@ -5,7 +5,7 @@ from apps.site.views.profile import handlers
 def install(app: FastAPI):
     router = APIRouter(tags=["server.profile"], prefix="/profile")
     router.add_api_route(
-        "/",
+        "/index",
         methods=["POST"],
         status_code=status.HTTP_200_OK,
         endpoint=handlers.get_profile
