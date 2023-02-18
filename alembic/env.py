c2928df57c24a2c3a815012203b8c5e264b627bf
cyrox007 <cyrox007@gmail.com>
2023-02-18 19:30:52 +0300

    fixed migration

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from apps.site.settings import Config as set_conf
+from settings import Config as set_conf
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from apps.site.components.users.model import User
+from components.users.model import User
 target_metadata = User.__table__.metadata
 
 # other values from the config, defined by the needs of env.py,
