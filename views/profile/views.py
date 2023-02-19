cb2c947e19fad2cd8a0e475ba99671d861232440
cyrox007 <cyrox007@gmail.com>
2023-02-19 11:27:26 +0300

    debug

--- views/profile/views.py
+++ views/profile/views.py
@@ -51,7 +51,7 @@ async def update_avatar(request: Request):
     response = await request.json()
     db_session = Database.connect_database()
 
-
+    print(response)
     filepath = temporary_saving(file=response["file"])
     avatar_path: dict = image_verification(filepath, filepath.split('.')[0]) # генерируем пути для сохранения
     
