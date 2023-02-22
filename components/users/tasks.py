d657cf6df7f4a9f47e440e792034ae392fa1b59a
cyrox007 <cyrox007@gmail.com>
2023-02-22 15:35:20 +0300

    fixed

--- components/users/tasks.py
+++ components/users/tasks.py
@@ -2,5 +2,6 @@ from run_celery import celery
 from components.users.email import email_verification
 
 
+@celery.task
 def email_verification_task(target, verification_link):
     email_verification(target, verification_link)
